b4554a6a22780f673fe27bb9f8c5abaed2277c5a
sentayhuJo <sentayhu.alim@gmail.com>
2021-04-19 01:18:42 +0100

    connection string updated

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.use('/api', reservations);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`)
+    console.log(`Server started at PORT:${PORT}`)
 });
